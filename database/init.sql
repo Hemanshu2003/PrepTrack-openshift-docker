a1e1b58b7603b53b70f4802c041d91849107b347
Hemanshu Waghmare <113788603+Hemanshu2003@users.noreply.github.com>
2026-02-20 16:38:47 +0530

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS study_tasks (
     id SERIAL PRIMARY KEY,
     topic VARCHAR(100) NOT NULL,
-    descriptions TEXT
+    description TEXT,
     status VARCHAR(20) DEFAULT 'Pending'
-)
+);
 
 INSERT INTO study_tasks (topic, description, status) VALUES 
 ('System Administration', 'Revise Linux and Windows server commands', 'In Progress'),
 ('Database Management', 'Practice advanced SQL queries and indexing', 'Pending'),
 ('Software Engineering', 'Review Python data structures and algorithms', 'Completed'),
 ('Containerization', 'Deploy multi-tier apps using Docker and Kubernetes', 'In Progress');
+
 
